20f39cac41542d47fa432b4971082f19d0539ef7
jonathanherman1 <jonathan.n.herman@gmail.com>
2024-11-22 16:50:31 -0500

    Fix bug: update env variables to use correct VITE references

--- src/api/postsApi.ts
+++ src/api/postsApi.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = import.meta.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
+const API_BASE_URL = import.meta.env.VITE_API_URL
+
 import type { Post } from '../schemas'
 
 type ApiResponseSuccess = {
