1d5d1332c139b72e04a91e5ec0710bdd1ecca76f
jonathanherman1 <jonathan.n.herman@gmail.com>
2024-11-24 00:59:43 -0500

    Add E2E test

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./test/setup.ts']
+    setupFiles: ['./test/setup.ts'],
+    exclude: ['**/node_modules/**', '**/dist/**', '**/cypress/**'],
   }
 })
